c76a5e403987cca09370548c28c678cc1791cff6
gmashtalyar <gmashtalyar@yandex.ru>
2024-01-22 21:34:55 +0300

    p1

---
+++ flappy_bird_tutorial.py
@@ -0,0 +1,38 @@
+import pygame, neat, time, os, random
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 800
+
+BIRD_IMAGES = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird3.png")))]
+PIPE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "pipe.png")))
+BASE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "base.png")))
+BACKGROUND_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bg.png")))
+
+
+class Bird:
+    IMAGES = BIRD_IMAGES
+    MAX_ROTATION = 25
+    ROTATION_VELOCITY = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.velocity = 0
+        self.height = self.y
+        self.image_count = 0
+        self.image = self.IMAGES[0]
+
+    def jump(self):
+        self.velocity = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        print("move")
+
+while True:
+    bird.move()
+
